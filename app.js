eb283a2cab14ba2d59e87219f5d1fe40d1fbc175
GallonShih <gallonshih0526@gmail.com>
2022-04-16 19:26:10 +0800

    feet: finish login feature

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -7,10 +9,18 @@ const PORT = 3000
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const usePassport = require('./config/passport')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -32,9 +42,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
