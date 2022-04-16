f52cc744cd3b30bd9e560957881308dd56a5419c
GallonShih <gallonshih0526@gmail.com>
2022-04-16 21:37:11 +0800

    feet: add detail & edit & delete feature

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+// 載入 express 並建構應用程式伺服器
 const express = require('express')
 const session = require('express-session')
 const passport = require('passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
+
 const bcrypt = require('bcryptjs')
+
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = 3000
+
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
-const usePassport = require('./config/passport')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
@@ -21,6 +27,17 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.error_msg = req.flash('error_msg')  // 設定 warning_msg 訊息
+  next()
+})
+
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -31,10 +48,44 @@ app.get('/', (req, res) => {
     .catch((error) => { return res.status(422).json(error) })
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+app.put('/todos/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  console.log(isDone === 'on')
+  return Todo.findByPk(id)
+    .then(todo => {
+      return todo.update({
+        name: name,
+        isDone: isDone === 'on'
+      })
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
